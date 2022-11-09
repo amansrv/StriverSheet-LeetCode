2ceac1c8c26b21d13d512986fb4c698a00b12460
Aman Srivastava <89743268+amansrv@users.noreply.github.com>
2022-11-09 15:23:28 +0530

    Add files via upload

---
+++ Dsa Striver Code/Day-17/Preorder/preorder.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+
+    // constructor
+    Node(int val)
+    {
+        this->data = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+void preorder(Node *root)
+{
+
+    if (root == NULL)
+    {
+        return;
+    }
+
+    cout << root->data << " ";
+    preorder(root->left);
+    preorder(root->right);
+}
+
+/*
+                1
+              /   \
+             2     3
+           /  \   /  \
+          4    5  6   7
+
+*/
+
+int main()
+{
+
+    Node *root = new Node(1);
+
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->right->left = new Node(6);
+    root->right->right = new Node(7);
+
+    cout << "The preOrder traversal for the binary tree is: " << endl;
+    preorder(root);
+    return 0;
+}
